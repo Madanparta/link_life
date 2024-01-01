c58e06c8eed6c96a3be743e26a555a77ccd93b1c
Madan k <partagowda15@gmail.com>
2024-01-02 01:26:20 +0530

    ..

--- frontend/src/utils/credentials.js
+++ frontend/src/utils/credentials.js
@@ -1,4 +1,4 @@
-export const BACKEND_API = 'https://link-life.onrender.com/'
+export const BACKEND_API = 'https://link-life.onrender.com'
 // export const BACKEND_API = 'http://localhost:5000'
 
 export const {token} = JSON.parse(localStorage.getItem("user")) || { token: null }
