1db086ca48d0a808f53d566966c0ad2e0a77e03c
Madan k <partagowda15@gmail.com>
2024-01-02 00:49:57 +0530

    update backend url

--- frontend/src/utils/credentials.js
+++ frontend/src/utils/credentials.js
@@ -1,4 +1,5 @@
-export const BACKEND_API = 'http://localhost:5000'
+export const BACKEND_API = 'https://link-life.onrender.com/'
+// export const BACKEND_API = 'http://localhost:5000'
 
 export const {token} = JSON.parse(localStorage.getItem("user")) || { token: null }
 export const userData = JSON.parse(localStorage.getItem("user")) || { user: null }
